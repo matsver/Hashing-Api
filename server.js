f6c98cb9721aa82dd5bb51acd50ffdc891d5a2ad
Mats <99052518@mydavinci.nl>
2019-01-04 23:44:07 +0100

    added routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const crypto = require('crypto-js');
 const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var port = process.env.PORT || 4000;
 
@@ -28,6 +33,33 @@ app.get('/api/hash/:string', (req, res) => {
     RIPEMD160
   });
 });
+
+// app.get('/api/hash/:string/:secret', (req, res) => {
+//   var string = req.params.string;
+//   var secret = req.params.secret;
+//   var PBKDF2 = crypto.PBKDF2(string, secret).toString();
+//   var AES = crypto.AES.encrypt(string, secret).toString();
+//   var tripledes = crypto.TripleDES.encrypt(string, secret).toString();
+//   var rc4 = crypto.RC4.encrypt(string, secret).toString();
+//   var rabbit = crypto.Rabbit.encrypt(string, secret).toString();
+//     res.status(200).json({
+//       string,
+//       PBKDF2,
+//       AES,
+//       tripledes,
+//       rc4,
+//       rabbit
+//     });
+// });
+
+app.post('/api/aes', (req, res) => {
+  var string = req.body.string;
+  var secret = req.body.secret;
+  var hashed = crypto.AES.encrypt(string, secret).toString();
+  res.json({hashed});
+});
+
+app.post('/api/')
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
